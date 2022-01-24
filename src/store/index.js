d414a243bacaa6d0bcf3aea52cde7cd89aacb8d7
Fabricio Carvalho <fcarvalho1999@gmail.com>
2022-01-24 17:48:46 -0300

    to do global state

--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,23 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    todos: []
   },
   mutations: {
+    loadTodos(state) {
+      state.todos = JSON.parse(localStorage.getItem('todos')) || []
+    },
+    addTodo(state, todo) {
+      state.todos = [{ content: todo, done: false }, ...state.todos]
+    },
+    removeTodo(state, todo) {
+      state.todos.splice(state.todos.indexOf(todo), 1)
+      localStorage.setItem('todos', JSON.stringify(state.todos))
+    },
+    toggleTodo(state, todo) {
+      todo.done = !todo.done
+      localStorage.setItem('todos', JSON.stringify(state.todos))
+    }
   },
   actions: {
   },
